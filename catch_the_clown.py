ed04933d9459db6db6827235e5e6e32e936e9aad
MattHosler <MatthewCHosler@gmail.com>
2022-12-15 18:51:46 -0500

    Finished catch the clown game

--- catch_the_clown.py
+++ catch_the_clown.py
@@ -108,6 +108,38 @@ while running:
     if clown_rect.top <= 0 or clown_rect.bottom >= WINDOW_HEIGHT:
         clown_dy = -1*clown_dy
 
+    #Update HUD
+    score_text = font.render("Score: " + str(score), True, YELLOW)
+    lives_text = font.render("Lives: " + str(player_lives), True, YELLOW)
+
+    #Check for game over
+    if player_lives == 0:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #Pause the game until the player clicks then reset the game
+        pygame.mixer.stop()
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+            #The player wants to play again
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+
+                    clown_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+                    clown_velocity = CLOWN_STARTING_VELOCITY
+                    clown_dx = random.choice([-1,1])
+                    clown_dy = random.choice([-1,1])
+
+                    pygame.mixer.music.play(-1, 0.0)
+                    is_paused = False
+                #The player wants to quit
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+            
     #Blit the background
     display_surface.blit(background_image, background_rect)
 
